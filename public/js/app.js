23ac9464f486eef5a0296bf8675c8e7ea275ad3d
YosseLine <yoseline_96@hotmail.com>
2018-02-16 18:24:54 -0500

    Agregando funciones

--- public/js/app.js
+++ public/js/app.js
@@ -12,5 +12,16 @@ form.addEventListener('submit', function(event) {
 
 function getNews() {
   const articleRequest = new XMLHttpRequest();
-  articleRequest.open('GET', );
+  articleRequest.open('GET', 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=<TU KEY>');
+  articleRequest.onload = addNews;
+  articleRequest.onerror = handleError;
+  articleRequest.send();
+}
+
+function handleError() {
+  console.log('Se ha presentado un error');
+}
+
+function addNews() {
+  console.log(this.responseText);
 }
